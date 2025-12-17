76cf7b7c7fb687f0f99946cd8a487891dcfaf594
SKB-TECH <shakokinyamba201@gmail.com>
2025-12-17 15:22:50 +0200

    configure docker

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { Toaster } from '@/components/ui/toaster';
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,6 +21,12 @@ export default function RootLayout({ children }) {
         <body className={inter.className}>
         <AuthProvider>
             {children}
+            <Script
+                src="https://skb-tech.github.io/botAI/dist/chatbot-widget.js"
+                strategy="afterInteractive"
+                data-faq="https://rmtlabs.ai/faq"
+                data-lang="fr"
+            />
             <Toaster />
         </AuthProvider>
         </body>
